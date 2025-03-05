87db26be034896199399af57eacd38b41561cb44
maxisandoval37 <maxisandoval37@users.noreply.github.com>
2025-03-05 22:31:42 +0000

    deploy: 5db49732adb3ec778382275052062efd7e82e7b6

--- src/main/java/ar/dev/maxisandoval/nextfix/repository/DirectorRepository.java
+++
@@ -1,9 +0,0 @@
-package ar.dev.maxisandoval.nextfix.repository;
-
-import ar.dev.maxisandoval.nextfix.model.Director;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface DirectorRepository extends JpaRepository <Director, Long> {
-}
--- src/main/java/ar/dev/maxisandoval/nextfix/repository/PlataformaRepository.java
+++
@@ -1,9 +0,0 @@
-package ar.dev.maxisandoval.nextfix.repository;
-
-import ar.dev.maxisandoval.nextfix.model.Plataforma;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface PlataformaRepository extends JpaRepository <Plataforma, Long> {
-}
